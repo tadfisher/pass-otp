tadfisher/pass-otp

commit 0aadd4c82cf190ae8ac9c08e7eb142716ed33a85

Add support for using Pass::OTP
Pass::OTP is alternative implementation of One-time passwords with more
features, namely it can support parsing otpauth:// links and custom
character classes. This can close #97

otp.bash
@@ -16,8 +16,9 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # []
 
-VERSION="1.1.1"
+VERSION="1.1.2"
 OATH=$(which oathtool)
+OTPTOOL=$(which otptool)
 
 ## source:  https://gist.github.com/cdown/1163649
 urlencode() {
@@ -335,6 +336,7 @@ cmd_otp_code() {
   contents=$($GPG -d "${GPG_OPTS[@]}" "$passfile")
   while read -r -a line; do
     if [[ "$line" == otpauth://* ]]; then
+      local uri="$line"
       otp_parse_uri "$line"
       break
     fi
@@ -348,6 +350,7 @@ cmd_otp_code() {
       [[ -n "$otp_period" ]] && cmd+=" --time-step-size=$otp_period"s
       [[ -n "$otp_digits" ]] && cmd+=" --digits=$otp_digits"
       cmd+=" $otp_secret"
+      [[ -n "$OTPTOOL" ]] && cmd="$OTPTOOL $uri"
       ;;
 
     hotp)
@@ -355,6 +358,7 @@ cmd_otp_code() {
       cmd="$OATH -b --hotp --counter=$counter"
       [[ -n "$otp_digits" ]] && cmd+=" --digits=$otp_digits"
       cmd+=" $otp_secret"
+      [[ -n "$OTPTOOL" ]] && cmd="$OTPTOOL $uri"
       ;;
 
     *)

commit 49e52ad7195e248eb79d58ccd2b9ed3d0e66ab1b

Do not fail when otptool is installed but oathtool is not installed
Fixes: commit 0aadd4c82cf190ae8ac9c08e7eb142716ed33a85

otp.bash
@@ -314,7 +314,7 @@ cmd_otp_append() {
 }
 
 cmd_otp_code() {
-  [[ -z "$OATH" ]] && die "Failed to generate OTP code: oathtool is not installed."
+  [[ -z "$OATH" && -z "$OTPTOOL" ]] && die "Failed to generate OTP code: oathtool or otptool is not installed."
 
   local opts clip=0 quiet=0
   opts="$($GETOPT -o cq -l clip,quiet -n "$PROGRAM" -- "$@")"