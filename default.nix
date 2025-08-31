tadfisher/pass-otp

commit 1e269f4d6e5451f4ca593d13f33f30c73dfe87bc

Use `command -v` instead of `which` for finding programs
which is non-standard and prints warnings when programs are not installed,
but pass-otp already handles programs not being installed properly.

Still use which in the tests because it usefully prints the $PATH too.

default.nix
@@ -8,7 +8,6 @@
 , gnupg
 , pass
 , shellcheck
-, which
 }:
 
 stdenv.mkDerivation {
@@ -33,7 +32,7 @@ stdenv.mkDerivation {
   doCheck = true;
 
   patchPhase = ''
-    sed -i -e 's|OATH=\$(which oathtool)|OATH=${oathToolkit}/bin/oathtool|' otp.bash
+    sed -i -e 's|OATH=\$(command -v oathtool)|OATH=${oathToolkit}/bin/oathtool|' otp.bash
   '';
 
   checkPhase = ''

otp.bash
@@ -17,8 +17,8 @@
 # []
 
 VERSION="1.1.2"
-OATH=$(which oathtool)
-OTPTOOL=$(which otptool)
+OATH=$(command -v oathtool)
+OTPTOOL=$(command -v otptool)
 
 if [[ $PASSAGE == 1 ]]; then
   EXT="age"

commit c12ea7495cccc516fb21863ebba75d351c08fa39

Fix Nix package
Was broken in 1e269f4d6e5451f4ca593d13f33f30c73dfe87bc

Resolved by adding back the which input, since the tests use it

default.nix
@@ -8,6 +8,7 @@
 , gnupg
 , pass
 , shellcheck
+, which
 }:
 
 stdenv.mkDerivation {